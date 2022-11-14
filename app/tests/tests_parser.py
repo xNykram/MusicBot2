f897351defb8435d29fd5bbb4785dda62fbf7810
Hetorx <arkadiuszkoziol2@gmail.com>
2022-11-14 19:30:18 +0100

    Files fixed by black and pylint

--- app/tests/tests_parser.py
+++ app/tests/tests_parser.py
@@ -1,16 +1,18 @@
 import pytest
-import second_parser
+from app.utility import second_parser
+
 
 def test_of_convertion():
-    assert second_parser.parse_duration(0) == '0s'
-    assert second_parser.parse_duration(1) == '1s'
-    assert second_parser.parse_duration (50) == '50s'
-    assert second_parser.parse_duration(60) == '1m'
-    assert second_parser.parse_duration (250) == '4m 10s'
-    assert second_parser.parse_duration(3600) == '1h'
-    assert second_parser.parse_duration (3601) == '1h 1s'
-    assert second_parser.parse_duration(86400) == '1d'
-    assert second_parser.parse_duration (86470) == '1d 1m 10s'
+    assert second_parser.parse_duration(0) == "0s"
+    assert second_parser.parse_duration(1) == "1s"
+    assert second_parser.parse_duration(50) == "50s"
+    assert second_parser.parse_duration(60) == "1m"
+    assert second_parser.parse_duration(250) == "4m 10s"
+    assert second_parser.parse_duration(3600) == "1h"
+    assert second_parser.parse_duration(3601) == "1h 1s"
+    assert second_parser.parse_duration(86400) == "1d"
+    assert second_parser.parse_duration(86470) == "1d 1m 10s"
+
 
 def test_seconds_raises():
     with pytest.raises(ValueError):
