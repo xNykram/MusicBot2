10b6f0daab477d753b2b71f202d4a8cc86e04a88
Hetorx <arkadiuszkoziol2@gmail.com>
2022-11-14 19:49:25 +0100

    precommit hook fixed

--- app/utility/second_parser.py
+++ app/utility/second_parser.py
@@ -1,17 +1,35 @@
 def parse_duration(sec: int) -> str:
     if sec < 0:
-        raise ValueError ("Given value cannot be less than 0!")
+        raise ValueError("Given value cannot be less than 0!")
     if sec == 0:
         return "0s"
     days = sec // 86400
     hours = (sec - days * 86400) // 3600
     minutes = (sec - days * 86400 - hours * 3600) // 60
     seconds = sec - days * 86400 - hours * 3600 - minutes * 60
-    result= ("{}d ".format(int(days)) if days else "" ) + \
-    ("{}h ".format(int(hours)) if hours else "" ) + \
-    ("{}m ".format(int(minutes)) if minutes else "" ) + \
-    ("{}s ".format(int(seconds)) if seconds else "")
-    if result == days or hours or minutes or seconds or days and not hours or days and not minutes and days and not seconds or hours and not minutes or hours and not seconds or minutes and not seconds:
-        return result [:-1]
+    result = (
+        ("{}d ".format(int(days)) if days else "")
+        + ("{}h ".format(int(hours)) if hours else "")
+        + ("{}m ".format(int(minutes)) if minutes else "")
+        + ("{}s ".format(int(seconds)) if seconds else "")
+    )
+    if (
+        result == days
+        or hours
+        or minutes
+        or seconds
+        or days
+        and not hours
+        or days
+        and not minutes
+        and days
+        and not seconds
+        or hours
+        and not minutes
+        or hours
+        and not seconds
+        or minutes
+        and not seconds
+    ):
+        return result[:-1]
     return result
-    
