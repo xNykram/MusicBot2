0290a8108c26a35de039ad90ba91600aa20108b7
xNykram <nykram@wp.pl>
2023-11-13 17:07:25 +0100

    - unit tests v6

--- app/tests/test_settings.py
+++ app/tests/test_settings.py
@@ -3,7 +3,7 @@ from app.core.settings import Settings
 
 def test_settings_class():
     settings = Settings()
-    
+
     assert settings.APP_ENV == "DEV"
     assert settings.BOT_PREFIX == "!"
     assert settings.BOT_TOKEN == ""
